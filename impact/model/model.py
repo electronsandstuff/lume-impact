028790ba84bd3d59fb19f0e77d52a1f28af3ca35
Christopher M. Pierce <contact@chris-pierce.com>
2026-07-09 13:08:01 -0700

    add docstrings

--- impact/model/model.py
+++ impact/model/model.py
@@ -12,6 +12,10 @@ from impact.model.config import VariableMappingConfig, make_actions
 
 
 class LUMEImpactModel(InitialParticlesMixIn, FinalParticlesMixIn, ActionModel[Impact]):
+    """
+    LUMEModel using the actions framework wrapping an Impact-T simulator object.
+    """
+
     def __init__(
         self,
         impact: Impact,
@@ -23,14 +27,38 @@ class LUMEImpactModel(InitialParticlesMixIn, FinalParticlesMixIn, ActionModel[Im
 
     @property
     def initial_particles(self) -> ParticleGroup:
+        """
+        Expose the initial particles provided to simulation using `InitialParticlesMixIn` for use in `StagedModel`.
+
+        Returns
+        -------
+        ParticleGroup
+            The starting particles from Impact-T
+        """
         return self.simulator.initial_particles
 
     @initial_particles.setter
     def initial_particles(self, val: ParticleGroup) -> None:
+        """
+        Expose the initial particles provided to simulation using `InitialParticlesMixIn` for use in `StagedModel`.
+
+        Parameters
+        ----------
+        val : ParticleGroup
+            The starting particles provided to Impact-T
+        """
         self.simulator.initial_particles = val
 
     @property
     def final_particles(self) -> ParticleGroup:
+        """
+        Expose the final particles provided to simulation using `FinalParticlesMixIn` for use in `StagedModel`.
+
+        Returns
+        -------
+        ParticleGroup
+            The final particles as annotated by Impact-T
+        """
         return self.simulator.particles.get("final_particles")
 
     @classmethod
@@ -40,6 +68,23 @@ class LUMEImpactModel(InitialParticlesMixIn, FinalParticlesMixIn, ActionModel[Im
         config: VariableMappingConfig = VariableMappingConfig(),
         **kwargs,
     ) -> "LUMEImpactModel":
+        """
+        Generate class populated with variables from an existing Impact-T object. Variable inclusion and naming is configured
+        through VariableMappingConfig.
+
+        Parameters
+        ----------
+        impact : Impact
+            Impact-T session with lattice loaded and already run (to populate stats data and particle groups)
+        config : VariableMappingConfig, optional
+            Variable creation config. Defines mapping from Impact-T elements, attributes to variables, by
+            default VariableMappingConfig()
+
+        Returns
+        -------
+        LUMEImpactModel
+            The generated model with action variables registered
+        """
         return cls(impact, make_actions(impact, config), **kwargs)
 
     def _set(self, values: dict[str, Any]) -> None:
